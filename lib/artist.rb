0c6f92401ef1f1e727897e109570b12a54416ed3
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-01 23:33:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,38 @@
-  class Artist 
-  
+  class Artist
   @@all = []
-  
-  attr_accessor :name, :songs  
-  
-  
+  attr_accessor :name, :songs
+
   def initialize(name)
-    @name = name 
+    @name = name
     @songs = []
-  end 
-  
-  
+  end
+
+  def self.all
+    @@all    
+  end
+
   def add_song(song)
-     self.songs << song 
-  end 
- 
-  
-  def save
-   @@all << self 
-  end 
-  
-  
-  def self.all 
-   @@all 
-  end 
-  
- 
-   
+    @songs << song
+  end
+
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
   end
-  
-  
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
   end
-  
-  
-  def self.find(name)
-    self.all.find { |name| name == name}
-  end 
-  
-  
-  def print_songs 
-    self.songs.each {|song| puts song.name}
+
+  def save
+    @@all << self
   end
-  end 
+
+  def print_songs
+    songs.each {|song| puts song}
+  end
+end
