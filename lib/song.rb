4fd16dcc369c01f26d1d644429420e7440c05d6d
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-01 23:12:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,12 @@
   end
   
   
-  def new_by_filename(file) 
-    song_data = file.chomp("mp3").split(" - ")
-    song = Song.new(song_data[1])
-    song.artist_name(song_data[0])
-  end 
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+   new_song.artist_name = artist
+   new_song
+  end
 
 
   end 
