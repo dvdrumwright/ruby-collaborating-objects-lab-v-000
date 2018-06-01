3b858ba5fc13b83d8cd617e4e4ff0f085268e7b9
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-01 23:25:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,13 +30,9 @@
     self.new(name).tap {|artist| artist.save}
   end
    
-  def self.find_or_create_by_name(name) 
-  if self.find(name)
-       self.find(name)
-  else 
-       self.create(name)
-  end 
-  end 
+ def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
   
   
   def self.find(name)
