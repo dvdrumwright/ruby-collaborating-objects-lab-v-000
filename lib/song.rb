0df73876b6d0df53b2cf1eb2f9abeaf94ef707a2
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-01 22:55:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,16 @@
   class Song 
   attr_accessor :name, :artist 
   
+  
   def initialize(name)
     @name = name
   end
 
   
-  
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   
   
   def new_by_filename(file) 
