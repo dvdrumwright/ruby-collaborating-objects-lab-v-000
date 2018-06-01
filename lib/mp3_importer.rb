4c540560ce77c86af1c27dcc5b63c90b886c3bcd
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-01 22:37:51 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,35 @@
-class MP3Importer 
+ class MP3Importer 
+   
+ attr_accessor :path 
+   
+ def initialize(path)
+     @path = path
+ end 
 
+ def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
 
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+     end
+    end
+   end
 
-
-end 
+   def files
+    Dir.new(self.path).map do |file|
+      files << file if file.length > 4
+   end
+  
+ 
+   def import 
+   self.files.each do |a| 
+     Song.new_by_filename(a)
+     end 
+    end 
+   end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,15 @@
-class Song 
+  class Song 
   attr_accessor :name, :artist 
   
   def initialize(name)
     @name = name
   end
 
+  def new_by_filename(file) 
+    song_data = file.chomp("mp3").split(" - ")
+    song = Song.new(song_data[1])
+    song.artist_name(song_data[0])
+  end 
 
 
-end 
+  end 
